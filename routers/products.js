fd62abd2cb1996aa921b632c8ef43c5d2eb8f6f9
supratim50 <supratimmondal50@gmail.com>
2021-06-03 20:33:10 +0530

    Add product image upload and delete route

--- routers/products.js
+++ routers/products.js
@@ -9,15 +9,33 @@ const Product = require("../models/product");
 let productImages = [];
 
 // upload product data
-route.get("/upload/data", auth, async (req, res) => {
+route.get("/upload/data", async (req, res) => {
   const product = new Product(req.body);
   try {
+    await product.save();
+    //images
+    productImages.map((buffer) => {
+      product.images = product.images.concat({ image: buffer });
+      productImages = [];
+    });
     await product.save();
     res.status(200).send(product);
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+// Fetch all product
+route.get(
+  "/",
+  async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error });
+  }
+);
 
 // Fetch product data
 route.get("/:id", async (req, res) => {
@@ -30,31 +48,44 @@ route.get("/:id", async (req, res) => {
 });
 
 // uploada images
-// const upload = multer({
-//   //   dest: "images",
-//   limits: {
-//     fileSize: 2000000,
-//   },
-//   fileFilter(req, file, cb) {
-//     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-//       return cb(new Error("Please insert an image!"));
-//     }
+const upload = multer({
+  limits: {
+    fileSize: 2000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please insert an image!"));
+    }
 
-//     cb(undefined, true);
-//   },
-// });
+    cb(undefined, true);
+  },
+});
 
 route.post(
   "/upload/image",
+  upload.single("upload"),
   async (req, res) => {
-    // console.log(req.file.buffer);
-    console.send("upload file");
-    res.send();
-    // res.send(productImages);
+    productImages.push(req.file.buffer);
+    res.send(productImages);
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error });
   }
 );
+
+// delete images from database
+route.delete("/delete/:productId/image/:imageID", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    product.images = product.images.filter((image) => {
+      return image._id != req.params.imageID;
+    });
+
+    await product.save();
+    res.send(product);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 module.exports = route;
